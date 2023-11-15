0bb4f70c776d7dcfdd7e668e193ee2b0e4441dd9
Yaseen Younus <yay200@gmail.com>
2023-11-14 23:30:45 -0600

    Adding S3 bucket CDK code

--- infra/infra/infra_stack.py
+++ infra/infra/infra_stack.py
@@ -1,11 +1,13 @@
-from aws_cdk import (
-    Stack
-)
+from aws_cdk import Stack, aws_s3 as s3
 from constructs import Construct
 
-class InfraStack(Stack):
 
+class InfraStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        
+        bucket_name = "justmathgames.com"
+
+        site_bucket = s3.Bucket.from_bucket_name(
+            self, "StaticSiteBucket", bucket_name=bucket_name
+        )
