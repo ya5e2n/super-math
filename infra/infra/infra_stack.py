32aba614e21f17c99e92e82c9a3fb31ad571bfa2
Yaseen Younus <yay200@gmail.com>
2023-11-15 00:53:25 -0600

    Updates to CDK and game code

--- infra/infra/infra_stack.py
+++ infra/infra/infra_stack.py
@@ -1,4 +1,4 @@
-from aws_cdk import Stack, aws_s3 as s3
+from aws_cdk import Stack, aws_s3 as s3, aws_s3_deployment as s3_deploy
 from constructs import Construct
 
 
@@ -6,8 +6,15 @@ class InfraStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        bucket_name = "justmathgames.com"
+        bucket_name = "testing.justmathgames.com"
 
         site_bucket = s3.Bucket.from_bucket_name(
             self, "StaticSiteBucket", bucket_name=bucket_name
+        )
+
+        s3_deploy.BucketDeployment(
+            self,
+            "BucketDeploy",
+            destination_bucket=site_bucket,
+            sources=[s3_deploy.Source.asset(path="../src")],
         )
--- src/script.js
+++ src/script.js
@@ -21,7 +21,7 @@ const number = document.querySelectorAll(".num"),
   version = document.querySelector("#version");
 
 // *** App Version ***
-version.innerHTML = "v0.0.29";
+version.innerHTML = "v0.0.30";
 
 var countCorrect = 0;
 var gameTimer = "";
