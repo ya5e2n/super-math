4476264c45f6eadd383ff57d2eee2e2a924191ff
Yaseen Younus <yay200@gmail.com>
2023-11-16 23:59:00 -0600

    Uncommenting live for now

--- infra/app.py
+++ infra/app.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
-import os
 
 import aws_cdk as cdk
 
 from infra.math_game import MathGame
 
-
 app = cdk.App()
+
 MathGame(
     app,
     "MathGameTestingSite",
     s3_bucket_name="testing.justmathgames.com"
 )
+
+# MathGame(
+#     app,
+#     "MathGameLiveSite",
+#     s3_bucket_name="justmathgames.com"
+# )
+
 
 app.synth()
